6970c9eb1d5196a241dfa7a9959e97e0e22def24
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2024-04-08 18:10:58 +0100

    home

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -4,8 +4,10 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background: linear-gradient(180deg, #000000 40%, rgb(4, 4, 40));
+    background: linear-gradient(80deg, #000000 50%, rgb(4, 4, 40));
     color: #fff;
+    margin: 0; /* Remove default margin */
+    height: 0; /* Ensure the body fills the viewport height */
   }
 `;
 
