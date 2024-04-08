93f7036eeae87da17ca2109c1f7af0a4103fdea0
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2024-04-09 00:38:46 +0100

    home

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -4,10 +4,9 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background: linear-gradient(80deg, #000000 50%, rgb(4, 4, 40));
+    background: linear-gradient(80deg, #000000 50%, #000000);
     color: #fff;
-    margin: 0; /* Remove default margin */
-    height: 0; /* Ensure the body fills the viewport height */
+   
   }
 `;
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,27 +6,29 @@ import logo from "../carta.jpg"
 function Home() {
   return (
     <>
-      <Container>
-        <section className="py-5">
-          <header>
-            <span>
-              <h1>CORTANA</h1>
-            </span>
+     
+        <section className="landing__page">
 
-            <div className="list__content">
-              <ul className="d-flex">
-                <li>
-                  <a href="/">Home</a>
-                </li>
-                <li>
-                  <a href="/">About</a>
-                </li>
-                <li>
-                  <a href="/">Socials</a>
-                </li>
-              </ul>
+          <div className="container content">
+
+            <h1>
+              AI -
+            </h1>
+
+            <div className="content__image">
+              <img
+              src={logo}
+                alt="cortana"
+                className="img-fluid"
+              />
             </div>
-          </header>
+
+           <span> <h1>
+              CORTANA
+            </h1></span>
+
+          </div>
+          
         </section>
 
         <section className="py-5">
@@ -38,16 +40,9 @@ function Home() {
               </h2>
             </div>
 
-            <div className="content__image">
-              <img
-              src={logo}
-                alt="cortana"
-                className="img-fluid"
-              />
-            </div>
+           
           </div>
         </section>
-      </Container>
     </>
   );
 }
