04b32e31ee75f9dba2f9192e58cbcb51a05d0f26
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2024-04-09 10:32:03 +0100

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,7 @@ import discord from "../images/discord.png";
 import tg from "../images/tg-logo.png";
 import x from "../images/x-logo.png";
 import intro from "../images/intro.mp4";
+import base from "../images/base.png";
 
 function Home() {
   return (
@@ -27,7 +28,9 @@ function Home() {
         </div>
       </section>
 
-      <section className="container py-5">
+      <section className="container int__welc py-5">
+
+        <img src={base} alt="base" className="img-fluid" />
         <p className="content__body">
           CORTANA is an innovative project aiming to revolutionize the gaming
           industry by introducing gamers to the world of cryptocurrency. The
@@ -42,21 +45,21 @@ function Home() {
 
       <section className="container py-1">
         <div className="socials">
-          <a href="https://x.com/CortanaCoin?t=4GFBZgpW0JxqvxbLXPh2Ww&s=08">
+          <a href="https://x.com/CortanaCoin?t=4GFBZgpW0JxqvxbLXPh2Ww&s=08" target="_blank" rel="noreferrer">
             {" "}
             <img src={x} alt="4" />
           </a>
-          <a href="https://t.me/+Q5M_PO8Tfms2ZDBh">
+          <a href="https://t.me/+Q5M_PO8Tfms2ZDBh" target="_blank" rel="noreferrer">
             <img src={tg} alt="4" />
           </a>
-          <a href="/">
+          <a href="/" target="_blank" rel="noreferrer">
             <img src={discord} alt="4" />
           </a>
 
-          <a href="/">
+          <a href="https://dexscreener.com/base/0x913ee36000bfe0ba10043a9ff5ed0d4680d35962" target="_blank" rel="noreferrer" >
             <img src={dex} alt="4" />
           </a>
-          <a href="/">
+          <a href="/" target="_blank" rel="noreferrer">
             <img src={jup} alt="4" />
           </a>
         </div>
@@ -70,7 +73,7 @@ function Home() {
           </h1>
         </div>
         <div className="text-center mt-5 intro__vide">
-          <video autoPlay loop>
+          <video autoPlay loop muted>
             <source src={intro} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
@@ -79,21 +82,21 @@ function Home() {
 
       <section className="container py-5">
         <div className="socials">
-          <a href="https://x.com/CortanaCoin?t=4GFBZgpW0JxqvxbLXPh2Ww&s=08">
+          <a href="https://x.com/CortanaCoin?t=4GFBZgpW0JxqvxbLXPh2Ww&s=08" target="_blank" rel="noreferrer">
             {" "}
             <img src={x} alt="4" />
           </a>
-          <a href="https://t.me/+Q5M_PO8Tfms2ZDBh">
+          <a href="https://t.me/+Q5M_PO8Tfms2ZDBh" target="_blank" rel="noreferrer">
             <img src={tg} alt="4" />
           </a>
-          <a href="/">
+          <a href="/" target="_blank" rel="noreferrer">
             <img src={discord} alt="4" />
           </a>
 
-          <a href="/">
+          <a href="https://dexscreener.com/base/0x913ee36000bfe0ba10043a9ff5ed0d4680d35962" target="_blank" rel="noreferrer">
             <img src={dex} alt="4" />
           </a>
-          <a href="/">
+          <a href="/" target="_blank" rel="noreferrer">
             <img src={jup} alt="4" />
           </a>
         </div>
