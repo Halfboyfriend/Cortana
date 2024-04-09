b78cba751ab268340f08f6fc7c25444c40d55666
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2024-04-09 23:22:31 +0100

    update

--- src/components/Token.js
+++ src/components/Token.js
@@ -5,28 +5,28 @@ import { VictoryPie } from 'victory';
 const Tokenomics = () => {
   const data = [
     { x: 'Total Supply', y: 25 },
-    { x: 'Reserve', y: 25 },
-    { x: 'Team & Advisors', y: 25 },
-    { x: 'Community Development', y: 25 },
-    { x: 'Ecosystem Fund', y: 25 },
+    { x: 'Reserve', y: 15 },
+    { x: 'Team & Advisors', y: 10},
+    { x: 'Community', y: 5 },
+    { x: 'Ecosystem Fund', y: 10 },
   ];
 
   return (
-    <Container maxW="xl" mt={8} mb={8}>
-      <Heading as="h2" size="xl" textAlign="center" mb={6}>
+    <Container maxW="xl" mt={0} mb={0}>
+      <Heading as="h2" size="xl" textAlign="center" mb={0}>
         Tokenomics
       </Heading>
-      <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={6}>
+      <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={0}>
         <Box p={6} boxShadow="lg">
           <VictoryPie
             data={data}
             colorScale={['#6B46C1', '#F56565', '#38B2AC', '#ED8936', '#fff']} // Example colors, adjust as needed
             innerRadius={70} // Adjust the size of the pie chart here
-            padding={40} // Adjust the padding here
+            padding={70} // Adjust the padding here
             style={{
               labels: {
-                fontSize: 12,
-                fill: 'black',
+                fontSize: 8,
+                fill: 'white',
               },
             }}
           />
